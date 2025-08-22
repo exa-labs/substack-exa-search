5d05510b48a60eb3537ee79ac7a257df6e95974f
Ishan Goswami <ishangoswami315@gmail.com>
2025-08-23 04:55:04 +0530

    working

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,8 @@ import SubstackFinder from "../components/SubstackFinder";
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-gradient-to-b from-white to-gray-50">
-      <div className="container mx-auto">
-        <SubstackFinder />
-      </div>
+    <div className="min-h-screen bg-gradient-to-b from-white to-gray-50 flex justify-center">
+      <SubstackFinder />
     </div>
   );
 }
